4b84e57fd634dc8846c47a35b8a248cb6ded7433
Ignat <ignatov_rs@abv.bg>
2025-02-20 19:20:15 +0200

    add insert categories migrations

---
+++ src/main/resources/db/migration/V2__RemoveAllCategories.sql
@@ -0,0 +1,3 @@
+TRUNCATE TABLE expense_category CASCADE;
+TRUNCATE TABLE income_category CASCADE;
+
---
+++ src/main/resources/db/migration/V3__InsertAllCategories.sql
@@ -0,0 +1,106 @@
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53f6'::uuid, 'home', 'Жилище');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53f7'::uuid, 'utilityBills', 'Комунални услуги');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53f8'::uuid, 'food', 'Храна и напитки');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53f9'::uuid, 'car', 'Автомобил');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53d2'::uuid, 'transport', 'Транспорт');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53d3'::uuid, 'health', 'Здраве');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53d4'::uuid, 'education', 'Образование');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53d5'::uuid, 'fun', 'Забавления и свободно време');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53d6'::uuid, 'trip', 'Пътувания и ваканции');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53d7'::uuid, 'personal', 'Лични разходи');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53d8'::uuid, 'family', 'Семейни разходи');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e53d9'::uuid, 'child', 'Грижи за деца');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e5389'::uuid, 'credits', 'Дългове и кредити');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e5390'::uuid, 'investments', 'Спестявания и инвестиции');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e5391'::uuid, 'donation', 'Благотворителност');
+
+INSERT INTO public.expense_category
+(id, "name", bg_name)
+VALUES('7ac171ce-96a8-41b9-b208-db45915e5392'::uuid, 'other', 'Други');
+
+
+
+
+INSERT INTO public.income_category
+(id, "name", bg_name)
+VALUES('45d4ce3b-3b23-4035-b63f-ca4211dba2d0'::uuid, 'salary', 'Заплата');
+
+INSERT INTO public.income_category
+(id, "name", bg_name)
+VALUES('50de22ce-ff62-4236-a299-d6632e077316'::uuid, 'bonus', 'Бонуси и премии');
+
+INSERT INTO public.income_category
+(id, "name", bg_name)
+VALUES('50de22ce-ff62-4236-a299-d6632e077317'::uuid, 'passive', 'Пасивни доходи');
+
+INSERT INTO public.income_category
+(id, "name", bg_name)
+VALUES('50de22ce-ff62-4236-a299-d6632e077319'::uuid, 'business', 'Бизнес приходи');
+
+INSERT INTO public.income_category
+(id, "name", bg_name)
+VALUES('50de22ce-ff62-4236-a299-d6632e077320'::uuid, 'projects', 'Доходи от странични проекти');
+
+INSERT INTO public.income_category
+(id, "name", bg_name)
+VALUES('50de22ce-ff62-4236-a299-d6632e077323'::uuid, 'social', 'Социални плащания');
+
+INSERT INTO public.income_category
+(id, "name", bg_name)
+VALUES('50de22ce-ff62-4236-a299-d6632e077327'::uuid, 'gifts', 'Подаръци');
+
+INSERT INTO public.income_category
+(id, "name", bg_name)
+VALUES('50de22ce-ff62-4236-a299-d6632e077328'::uuid, 'purchase', 'Продажба на вещи');
+
+INSERT INTO public.income_category
+(id, "name", bg_name)
+VALUES('50de22ce-ff62-4236-a299-d6632e077329'::uuid, 'recovered', 'Възстановени средства');
+
+INSERT INTO public.income_category
+(id, "name", bg_name)
+VALUES('f3b93e0c-0c29-4044-9ee2-aef98c6b2a15'::uuid, 'other', 'Други');
