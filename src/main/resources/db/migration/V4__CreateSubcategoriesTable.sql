4d471ebc7b6a73ef6462b47f76c690f812648d0a
Ignat <ignatov_rs@abv.bg>
2025-02-22 15:10:56 +0200

    migrations changes

---
+++ src/main/resources/db/migration/V4__CreateSubcategoriesTable.sql
@@ -0,0 +1,18 @@
+CREATE TABLE subcategory (
+    id UUID PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    bg_name VARCHAR(255) NOT NULL,
+    income_category_id UUID,
+    expense_category_id UUID,
+    CONSTRAINT fk_income_category
+        FOREIGN KEY (income_category_id)
+        REFERENCES income_category (id)
+        ON DELETE SET NULL,
+    CONSTRAINT fk_expense_category
+        FOREIGN KEY (expense_category_id)
+        REFERENCES expense_category (id)
+        ON DELETE SET NULL
+);
+
+CREATE INDEX idx_income_category_id ON subcategory (income_category_id);
+CREATE INDEX idx_expense_category_id ON subcategory (expense_category_id);
