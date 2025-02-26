d3e6a6f98fad83cc291ce381a38d3c2c216b1d92
Ignat <ignatov_rs@abv.bg>
2025-02-26 10:00:54 +0200

    refactoring

--- src/main/java/com/ludogorieSoft/budgetnik/service/TokenService.java
+++ src/main/java/com/ludogorieSoft/budgetnik/service/TokenService.java
@@ -7,14 +7,19 @@ import com.ludogorieSoft.budgetnik.model.enums.TokenType;
 import java.util.List;
 
 public interface TokenService {
-    AuthResponse generateAuthResponse(User user);
-    Token findByToken(String jwt);
-    List<Token> findByUser(User user);
-    void saveToken(User user, String jwtToken, TokenType tokenType);
-//    void revokeToken(Token token);
-//    void revokeAllUserTokens(User user);
-    void logoutToken(String jwt);
-    void saveToken(Token token);
-    Token getLastToken(User user, TokenType tokenType);
-    void saveExpiredToken(Token token);
+  AuthResponse generateAuthResponse(User user);
+
+  Token findByToken(String jwt);
+
+  List<Token> findByUser(User user);
+
+  void saveToken(User user, String jwtToken, TokenType tokenType);
+
+  void logoutToken(String jwt);
+
+  void saveToken(Token token);
+
+  Token getLastToken(User user, TokenType tokenType);
+
+  void saveExpiredToken(Token token);
 }
